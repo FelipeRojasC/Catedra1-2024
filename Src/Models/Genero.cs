242a46e86a3b1622e973ac46d069ac97cff2ae39
FelipeRojasC <felipe.rojas02@alumnos.ucn.cl>
2024-10-02 19:59:55 -0300

    feat: added models in project

---
+++ Src/Models/Genero.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace catedra1_api.Src.Models
+{
+    public class Genero
+    {
+        public int id {get;set;}
+        public string name {get;set;}
+        
+    }
+}
---
+++ Src/Models/Usuario.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace catedra1_api.Src.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int id {get;set;}
+        public string rut {get;set;}
+        public string nombre {get;set;}
+        public string correo {get;set;}
+        public DataType fechaNachimiento {get;set;}
+
+        //relaciones
+
+        public int generoId {get;set;}
+        public Genero genero {get;set;}
+    }
+}
