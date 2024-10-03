6b52d030e02144298e06a9b1df97af3cb266fc33
FelipeRojasC <103686460+FelipeRojasC@users.noreply.github.com>
2024-10-02 23:51:40 -0300

    fix: fixed dataseeders

--- Src/Data/DataSeeders.cs
+++ Src/Data/DataSeeders.cs
@@ -33,8 +33,98 @@ namespace catedra1_api.Src.Data.Migrations
                         fechaNachimiento = new DateTime(2000,10,25)
                     };
                     context.Usuarios.Add(usuario);
+                    context.SaveChanges();
+
+                    usuario = new Usuario {  
+                        rut = "19524884k",
+                        nombre = "Oscar",
+                        correo = "correo2@gmail.com",
+                        generoId = 1,
+                        fechaNachimiento = new DateTime(1990,09,14)
+                    };
+                    context.Usuarios.Add(usuario);
+                    context.SaveChanges();
+
+                    usuario = new Usuario {  
+                        rut = "219588471",
+                        nombre = "Javiera",
+                        correo = "correo3@gmail.com",
+                        generoId = 2,
+                        fechaNachimiento = new DateTime(2000,01,04)
+                    };
+                    context.Usuarios.Add(usuario);
+                    context.SaveChanges();
+
+                    usuario = new Usuario {  
+                        rut = "21454471k",
+                        nombre = "Nicolas",
+                        correo = "correo4@gmail.com",
+                        generoId = 3,
+                        fechaNachimiento = new DateTime(1986,12,31)
+                    };
+                    context.Usuarios.Add(usuario);
+                    context.SaveChanges();
+
+                    usuario = new Usuario {  
+                        rut = "94237483",
+                        nombre = "Cristina",
+                        correo = "correo5@gmail.com",
+                        generoId = 2,
+                        fechaNachimiento = new DateTime(1996,08,14)
+                    };
+                    context.Usuarios.Add(usuario);
+                    context.SaveChanges();
+
+                    usuario = new Usuario {  
+                        rut = "19544123k",
+                        nombre = "Susana",
+                        correo = "correo6@gmail.com",
+                        generoId = 4,
+                        fechaNachimiento = new DateTime(1978,04,05)
+                    };
+                    context.Usuarios.Add(usuario);
+                    context.SaveChanges();
+
+                    usuario = new Usuario {  
+                        rut = "195679756",
+                        nombre = "Constanza",
+                        correo = "correo7@gmail.com",
+                        generoId = 2,
+                        fechaNachimiento = new DateTime(1997,01,04)
+                    };
+                    context.Usuarios.Add(usuario);
+                    context.SaveChanges();
+
+                    usuario = new Usuario {  
+                        rut = "175900081",
+                        nombre = "Jose",
+                        correo = "correo8@gmail.com",
+                        generoId = 4,
+                        fechaNachimiento = new DateTime(1992,03,25)
+                    };
+                    context.Usuarios.Add(usuario);
+                    context.SaveChanges();
+
+                    usuario = new Usuario {  
+                        rut = "18542963k",
+                        nombre = "Francisca",
+                        correo = "correo9@gmail.com",
+                        generoId = 1,
+                        fechaNachimiento = new DateTime(2000,10,25)
+                    };
+                    context.Usuarios.Add(usuario);
+                    context.SaveChanges();
+
+                    usuario = new Usuario {  
+                        rut = "105200851",
+                        nombre = "Marta",
+                        correo = "correo10@gmail.com",
+                        generoId = 1,
+                        fechaNachimiento = new DateTime(2010,11,25)
+                    };
+                    context.Usuarios.Add(usuario);
+                    context.SaveChanges();
                 }
-                context.SaveChanges();
             }
             
         }
